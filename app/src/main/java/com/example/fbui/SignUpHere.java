ca9585082ed1e5ea5ea896c6f45be44e3e86f029
karki-03 <bidutjava3@gmail.com>
2020-09-04 20:11:44 +0530

    FB commit_two

---
+++ app/src/main/java/com/example/fbui/SignUpHere.java
@@ -0,0 +1,14 @@
+package com.example.fbui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpHere extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_here);
+    }
+}
